cfb2ddc89e0775e855c141d383cdb2d68969f0fe
Sketney <134638115+Sketney@users.noreply.github.com>
2025-10-24 22:12:43 +0300

    Update not-found.tsx

--- apps/web/app/not-found.tsx
+++ apps/web/app/not-found.tsx
@@ -1,3 +1,5 @@
+export const dynamic = 'force-dynamic' // <— ключевая строка
+
 export default function NotFound() {
-  return <main style={{padding:16}}>Страница не найдена</main>
+  return <main style={{padding:16,fontFamily:'system-ui'}}>Страница не найдена</main>
 }
